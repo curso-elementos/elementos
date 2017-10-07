2aeeb06eb955a6d2de0a3e3102f42b1806f9b5b0
como-quesito <rapidclimate@outlook.com>
2017-10-06 21:39:04 -0500

    sas

---
+++ src/main/java/org/unitec/elementos2017/tablitas.sql
@@ -0,0 +1,8 @@
+
+-- generamos nuestra tablita
+create table mensajito (id integer primary key auto_increment,
+                       titulo varchar(30),
+                       cuerpo varchar(250)
+);
+
+select * from mensajito;
